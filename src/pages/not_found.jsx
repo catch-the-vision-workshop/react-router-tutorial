0801e63a0e4f405176b5fd2df970f60ee6e1e9fd
chinathaip <cartoonabe@gmail.com>
2024-01-17 14:57:01 +0700

    useNavigate hook

--- src/pages/not_found.jsx
+++ src/pages/not_found.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-  return <h1>Page Not Found!</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+      // navigate(-1) essentially returns to the previous page
+      // navigate(-2) returns to the 2nd previous page
+    }, 1000);
+  }, []);
+
+  return <h1>Page Not Found! Going back to home page in 1 second</h1>;
 }
